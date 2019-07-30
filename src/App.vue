f94e41413e9b5ce531f68e189b19e8dd13fd511a
JackieAnjie <liu@janatechnology.com>
2019-07-30 10:52:46 -0400

    started vue practice project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin:0;
+  padding:0;
+}
+
+body {
+  font-family:Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>Side bar</h1>
+        <div class=sidebar>
+            <a v-bind:key="item.id" v-for="item in sidebar">{{item.msg}}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Sidebar",
+    data() {
+        return {
+            sidebar: [
+                {
+                    id: 1,
+                    msg:"Option 1"
+                },
+                {
+                    id:2,
+                    msg:"Option 2"
+                },
+                {
+                    id: 3,
+                    msg:"Option 3"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+}
+
+.sidebar {
+    height: 100%;
+    width: 160px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+</style>
